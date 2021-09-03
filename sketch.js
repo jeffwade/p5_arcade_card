f7e4743dc45b9f8459d1bc5de02e0cc628e9e752
Jeff Wade <jeffwade@users.noreply.github.com>
2021-09-02 18:42:49 -0700

    add colors, fonts, css

--- sketch.js
+++ sketch.js
@@ -1,8 +1,38 @@
+// Arcade Card
+// An interactive card / rave display
+
+// Global variables
+let headlineFont, utilityFont;
+let headline = 'Happy birthday to you.';
+let c;
+
+
+function preload() {
+  c = loadJSON("colors.json");
+  headlineFont = loadFont('assets/IBMPlexSerif-Bold.otf');
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(240);
+  let card = createCanvas(windowWidth, windowHeight);
+  card.style('display', 'block');
+
+  colorMode(HSB);
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
+  background(c.teal.h, c.teal.s, c.teal.b, 0.3);
+  // drawText(headlineFont, headline);
   ellipse(mouseX, mouseY, 20, 20);
+}
+
+function drawText(font) {
+  // fill()
+  fill('#aaa');
+  textSize(48);
+  text(headline, 200, 200);
 }
